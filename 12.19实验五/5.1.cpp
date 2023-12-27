a73881cce4d0260e4b72253a163ae3e5fddb4436
Qi021212 <150007184+Qi021212@users.noreply.github.com>
2023-12-27 15:58:23 +0800

    Add files via upload

---
+++ 12.19实验五/5.1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Time       
+{
+private:
+	int hour, minute, sec;
+public:     
+	void set( )
+	{
+		cin >> hour >> minute >> sec;
+		cout << hour << ":" << minute << ":" << sec << endl;
+	}
+	
+};
+int main()
+{
+	Time t1;
+	t1.set(); 
+	return 0;
+}
---
+++ 12.19实验五/main.cpp
@@ -0,0 +1,12 @@
+//main.cpp
+#include<iostream>
+#include"student.h"
+using namespace std;
+int main()
+{
+	Student stud;
+	Student stud1(007, "tcg", 'm');
+	stud.display();
+	stud1.display();
+	return 0;
+}
